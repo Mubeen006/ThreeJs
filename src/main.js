1232e8c4379a0a31fc165fecb454c9499a6cc011
Mubeen006 <cmubeen599@gmai.com>
2025-02-14 17:50:50 +0500

    orbit controls

--- src/main.js
+++ src/main.js
@@ -1,32 +1,47 @@
-import './style.css'
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
+
+// orbit control provides the ability to move the camera around
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Wireframe } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 , wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
-const canvas= document.getElementById('canvas');
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
+const canvas = document.getElementById("canvas");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // to make the design responsive
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   // one point to note down is that whenever we change camera values we need to update the projection matrix
   camera.updateProjectionMatrix();
-})
+});
 
-let clock= new THREE.Clock();
+// lets make control on the movement of cuve
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // to make the movement smooth
+controls.autoRotate = true; // this will rotate the cube
+controls.autoRotateSpeed = 4.0; // the speed of rotation
+controls.dampingFactor = 0.02; // the speed of damping/rotation stop
+
 function animate() {
-  requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  // also updata controls on each frame
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
